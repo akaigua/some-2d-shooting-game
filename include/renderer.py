0f6ec98c0c8e5fdd4450c8b1e3af0f2e5adac28b
Song BoYao <aaKaiGua@gmail.com>
2021-07-27 09:59:12 +0800

    Updated include

--- include/renderer.py
+++ include/renderer.py
@@ -5,4 +5,4 @@ def render(r: dict):
     # The r dict should be looked like: {(tuple contain the coordinate of character) : the asset name that needs to be
     # rendered as a string}
     for key, value in r:
-        pass
+        assert type(key) == tuple, "The coordinate must be in a tuple that looks like (x,y)"
