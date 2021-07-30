6a6e55e274d9862b350838b48bc7afa49cc6dafe
AlpacaForever <87977366+AlpacaForever@users.noreply.github.com>
2021-07-30 11:06:45 +0800

    Update rooms.py

--- include/rooms.py
+++ include/rooms.py
@@ -35,3 +35,19 @@ class Map:
         self.rect_list = []
         for i in range(len(nList)):
             self.rect_list.append(pygame.Rect(nList[i][0] * 576, nList[i][1] * 384, nList[i][2] * 576, nList[i][3] * 384))
+
+    def map_2(self):
+        nlist = [[0,0,2,1],[0,1,1,1],[0,5,2,1],[0,6,1,1],[4,3,7,1],[3,8,1,3],[4,8,2,1],[5,6,1,2],[8,7,5,1],[10,8,1,1],
+                 [13,0,1,4],[14,3,2,1],[15,6,3,1],[17,7,1,4],[14,9,2,1],[0,11,18,1]]
+        self.rect_list = []
+        for i in range(len(nlist)):
+            self.rect_list.append(
+                pygame.Rect(nlist[i][0] * 32, nlist[i][1] * 32, nlist[i][2] * 32, nlist[i][3] * 32))
+
+    def map_3(self):
+        nlist = [[0,0,1,3],[1,2,3,1],[0,5,1,2],[3,8,1,3],[4,8,3,1],[6,5,2,1],[7,3,1,2],[8,3,5,1],[12,2,1,1],[9,7,5,1],
+                 [15,5,3,1],[17,2,1,4],[16,10,2,1],[17,9,1,1],[0,11,18,1]]
+        self.rect_list = []
+        for i in range(len(nlist)):
+            self.rect_list.append(
+                pygame.Rect(nlist[i][0] * 32, nlist[i][1] * 32, nlist[i][2] * 32, nlist[i][3] * 32))
