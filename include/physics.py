2fdad11535a96c0699722e2dfad98d47e069aa6e
akaigua <85090456+akaigua@users.noreply.github.com>
2021-08-03 09:00:39 +0800

    Update physics.py

--- include/physics.py
+++ include/physics.py
@@ -14,19 +14,16 @@ class physics:
         self.c = c
         self.in_air_time = 0
 
-    def __in_air(self, lat, m: include.rooms.Room, c_bottom):
+    def __in_air(self, lat, m: include.rooms.Room):
         h = self.c.status_avatar.get_height()
         w = self.c.status_avatar.get_width()
         # print((int((self.c.x + w)/WIDTH*18-1), (int((self.c.y + h)/HEIGHT*12))))
-        # if  m.room_structure.get((int((self.c.x + w) / WIDTH * 18 - 1),\
-           # (int((self.c.y + h) / HEIGHT * 12))), None) or 
-        if c_bottom:
+        if m.room_structure.get((int((self.c.x + w) / WIDTH * 18 - 1), (int((self.c.y + h) / HEIGHT * 12))), None):
             self.in_air_time = 0
         else:
             self.in_air_time += lat
 
 
-    def physic_handling(self, latency: float,m:include.rooms.Room,c_bottom):
-        self.__in_air(latency,m,c_bottom)
+    def physic_handling(self, latency: float,m:include.rooms.Room):
+        self.__in_air(latency,m)
         return self.c.y + 0.25 * G * (self.in_air_time**2)
-
