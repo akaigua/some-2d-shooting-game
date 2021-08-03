59cfb3c2984828edf3004ef6d11355d0beddd6fc
akaigua <85090456+akaigua@users.noreply.github.com>
2021-08-03 10:52:48 +0800

    Update rooms.py

--- include/rooms.py
+++ include/rooms.py
@@ -1,7 +1,9 @@
+from typing import *
+import random
 import pygame
 import os
 import numpy as np
-import include.Character
+
 PIC_WIDTH = 576
 PIC_HEIGHT = 384
 
@@ -10,51 +12,30 @@ PIC_HEIGHT = 384
 
 class Room:
     terrain_location = {
-        1: {(0, 3): True, (0, 4): True, (0, 5): True, (0, 11): True, (1, 3): True}
+        1: {(0, 3): True, (0, 4): True, (0, 5): True, (0, 11): True, (1, 3): True, (2, 3): True, (3, 7): True,
+            (4, 7): True,
+            (5, 7): True, (6, 4): True, (7, 4): True, (8, 4): True, (8, 5): True, (8, 6): True, (8, 7): True,
+            (8, 8): True,
+            (8, 9): True, (8, 10): True, (11, 1): True, (11, 2): True, (11, 3): True, (12, 3): True, (13, 3): True,
+            (14, 3): True,
+            (12, 7): True, (13, 7): True, (14, 7): True, (15, 7): True, (16, 10): True, (17, 10): True, (17, 9): True,
+            (1, 11): True,
+            (2, 11): True, (3, 11): True, (4, 11): True, (5, 11): True, (6, 11): True, (7, 11): True, (8, 11): True,
+            (9, 11): True,
+            (10, 11): True, (11, 11): True, (12, 11): True, (13, 11): True, (14, 11): True, (15, 11): True,
+            (16, 11): True, (17, 11): True
+            },
     }
 
     def __init__(self, room_ID: int):
         self.room_id = room_ID
         self.room_structure = Room.terrain_location[1]
 
-    def direction(self, rect1, rect2):
-        if rect1.midtop[1] > rect2.midtop[1]:
-            # print('up')
-            return 'up'
-        if rect1.midleft[0] > rect2.midleft[0]:
-            # print('left')
-            return 'left'
-        if rect1.midright[0] < rect2.midright[0]:
-            # print('right')
-            return 'right'
-        if rect1.midbottom[1] < rect2.midbottom[1]:
-            # print('bottom')
-            return 'bottom'
-
-    def collide(self, nList, c: include.Character.Player):
-        self.rect_list = []
-        for i in range(len(nList)):
-            self.rect_list.append(pygame.Rect(nList[i][0] * 32, nList[i][1] * 32, nList[i][2] * 32, nList[i][3] * 32))
-
-        print(self.rect_list)
-        print(c.rect)
-
-        for i in range(len(self.rect_list)):
-            print("fored")
-            print(c.rect.colliderect(self.rect_list[i]))
-            if c.rect.colliderect(self.rect_list[i]) == True:
-                Room.direction(self, c.rect, self.rect_list[i])
-                print("ifed")
-            else:
-                print("elsed")
-                return False, False, False, False
-
 
 class Map:
 
     def __init__(self):
-        # map_list = []
-
+        map_list = []
         self.rect_list = []
 
     def map_1(self):
@@ -63,27 +44,6 @@ class Map:
                  [16, 11, 2, 1], [0, 11, 18, 1], [3, 7, 3, 1],
                  [12, 7, 4, 1], [11, 1, 1, 3],
                  [12, 3, 3, 1]]
-
-        return nList
-
-        '''''
-        self.rect_list = []
-        for i in range(len(nList)):
-            self.rect_list.append(pygame.Rect(nList[i][0] * 32, nList[i][1] * 32, nList[i][2] * 32, nList[i][3] * 32))
-
-        for i in self.rect_list:
-            if c.rect.colliderect(self.rect_list[i]) != -1:
-                Room.direction(c.rect, self.rect_list[i])
-        '''''
-
-    def map_2(self):
-        nList = [[0, 0, 2, 1], [0, 1, 1, 1], [0, 5, 2, 1], [0, 6, 1, 1], [4, 3, 7, 1], [3, 8, 1, 3], [4, 8, 2, 1],
-                 [5, 6, 1, 2], [8, 7, 5, 1], [10, 8, 1, 1],
-                 [13, 0, 1, 4], [14, 3, 2, 1], [15, 6, 3, 1], [17, 7, 1, 4], [14, 9, 2, 1], [0, 11, 18, 1]]
-
-        return nList
-        '''''
-=======
         self.rect_list = []
         for i in range(len(nList)):
             self.rect_list.append(
@@ -93,27 +53,16 @@ class Map:
         nlist = [[0, 0, 2, 1], [0, 1, 1, 1], [0, 5, 2, 1], [0, 6, 1, 1], [4, 3, 7, 1], [3, 8, 1, 3], [4, 8, 2, 1],
                  [5, 6, 1, 2], [8, 7, 5, 1], [10, 8, 1, 1],
                  [13, 0, 1, 4], [14, 3, 2, 1], [15, 6, 3, 1], [17, 7, 1, 4], [14, 9, 2, 1], [0, 11, 18, 1]]
->>>>>>> Stashed changes
         self.rect_list = []
         for i in range(len(nlist)):
             self.rect_list.append(
                 pygame.Rect(nlist[i][0] * 32, nlist[i][1] * 32, nlist[i][2] * 32, nlist[i][3] * 32))
-        '''''
 
     def map_3(self):
-        nList = [[0, 0, 1, 3], [1, 2, 3, 1], [0, 5, 1, 2], [3, 8, 1, 3], [4, 8, 3, 1], [6, 5, 2, 1], [7, 3, 1, 2],
-                 [8, 3, 5, 1], [12, 2, 1, 1], [9, 7, 5, 1],
-                 [15, 5, 3, 1], [17, 2, 1, 4], [16, 10, 2, 1], [17, 9, 1, 1], [0, 11, 18, 1]]
-        return nList
-        '''''
-=======
         nlist = [[0, 0, 1, 3], [1, 2, 3, 1], [0, 5, 1, 2], [3, 8, 1, 3], [4, 8, 3, 1], [6, 5, 2, 1], [7, 3, 1, 2],
                  [8, 3, 5, 1], [12, 2, 1, 1], [9, 7, 5, 1],
                  [15, 5, 3, 1], [17, 2, 1, 4], [16, 10, 2, 1], [17, 9, 1, 1], [0, 11, 18, 1]]
->>>>>>> Stashed changes
         self.rect_list = []
         for i in range(len(nlist)):
             self.rect_list.append(
                 pygame.Rect(nlist[i][0] * 32, nlist[i][1] * 32, nlist[i][2] * 32, nlist[i][3] * 32))
-<<<<<<< Updated upstream
-        '''''
